ceff5274f40503708f136c8f66d4738648d80478
Gordon Fountain <Gordonthecellist@gmail.com>
2024-08-04 09:49:46 -0400

    Work on mouse capture test

--- extra_scripts/print_mouse_outputs.py
+++ extra_scripts/print_mouse_outputs.py
@@ -1,12 +1,10 @@
-import io
-import serial
+from pynput import mouse
 
-ser = serial.Serial('/dev/hidraw0', 9600, timeout=5.0)
-sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+def on_move(x, y):
+    print('Pointer moved to {0}'.format(
+        (x, y)))
 
-while ser.in_waiting > 0:  # While there are bytes in the input buffer
-    try:
-        line = sio.readline()
-        print(line + '\n')
-    except serial.SerialException as e:
-        print('Mouse Device error: {}'.format(e))
+listener = mouse.Listener(
+    on_move=on_move)
+listener.start()
+
