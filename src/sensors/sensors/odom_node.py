ff6435e89e6e53769dc360cb6c5586e1760bda9a
Gordon Fountain <Gordonthecellist@gmail.com>
2024-09-10 20:31:49 -0400

    Only statically publish once

--- src/sensors/sensors/odom_node.py
+++ src/sensors/sensors/odom_node.py
@@ -47,21 +47,22 @@ class OdomNode(Node):
         self.link_2_laser_broadcaster = StaticTransformBroadcaster(self)
 
         # TF from base link to laser is static, so define once
-        self.laser_tf_msg = TransformStamped()
-        self.laser_tf_msg.header.stamp = self.get_clock().now().to_msg()
-        self.laser_tf_msg.header.frame_id = 'base_link'
-        self.laser_tf_msg.child_frame_id = 'base_laser'
+        laser_tf_msg = TransformStamped()
+        laser_tf_msg.header.stamp = self.get_clock().now().to_msg()
+        laser_tf_msg.header.frame_id = 'base_link'
+        laser_tf_msg.child_frame_id = 'base_laser'
 
         # for now assuming laser is at the center of the bot
-        self.laser_tf_msg.transform.translation.x = 0.0
-        self.laser_tf_msg.transform.translation.y = 0.0
-        self.laser_tf_msg.transform.translation.z = 0.0
+        laser_tf_msg.transform.translation.x = 0.0
+        laser_tf_msg.transform.translation.y = 0.0
+        laser_tf_msg.transform.translation.z = 0.0
 
         # orientation of straight forward
-        self.laser_tf_msg.transform.rotation.x = 0.0
-        self.laser_tf_msg.transform.rotation.y = 0.0
-        self.laser_tf_msg.transform.rotation.z = 0.0
-        self.laser_tf_msg.transform.rotation.w = 0.0
+        laser_tf_msg.transform.rotation.x = 0.0
+        laser_tf_msg.transform.rotation.y = 0.0
+        laser_tf_msg.transform.rotation.z = 0.0
+        laser_tf_msg.transform.rotation.w = 1.0
+        self.link_2_laser_broadcaster.sendTransform(laser_tf_msg) # only publish msg once as it is static
 
         self.status_publisher = self.create_publisher(NodeStatus, 'odom_node/status', 10)
         self.status_msg = NodeStatus()
@@ -107,7 +108,6 @@ class OdomNode(Node):
         tf_msg.transform.rotation.w = q[3]
 
         self.odom_2_link_broadcaster.sendTransform(tf_msg)
-        self.link_2_laser_broadcaster.sendTransform(self.laser_tf_msg)
 
 def main(args=None):
     rclpy.init(args=args)
